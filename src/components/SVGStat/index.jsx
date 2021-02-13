314c550717b91b5b9ea7c990c8f4e2886832bdbd
sunyunxian <personal_sunyunxian@foxmail.com>
2021-02-14 00:47:10 +0800

    delete grid.svg fiel

--- src/components/SVGStat/index.jsx
+++ src/components/SVGStat/index.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import GitHubSvg from 'assets/github.svg';
-import GridSvg from 'assets/grid.svg';
+// import GridSvg from 'assets/grid.svg';
 import styles from './style.module.scss';
 
 const SVGStat = () => (
   <div>
     <GitHubSvg className={styles.runSVG} />
-    <GridSvg className={styles.runSVG} />
+    {/* <GridSvg className={styles.runSVG} /> */}
   </div>
 );
 
